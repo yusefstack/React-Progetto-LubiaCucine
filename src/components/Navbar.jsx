441dabc862d23bc489a0215ee45a2064356a9873
yusefstack <giustack02@gmail.com>
2023-05-08 18:05:02 +0200

    Ritocchi minimi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,9 +22,6 @@ const Navbar = () => {
           </svg>
           <div className='absolute left-6 top-8 py-0.5 px-2 text-center bg-purple-300 rounded-full'><p className='text-purple-600'>{totalItems}</p></div>
         </Link>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12 text-white">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
-        </svg>
       </div>
     </nav>
   )
